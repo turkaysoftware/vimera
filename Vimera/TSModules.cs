322c753ab3d7432da92eed8c0d8f6f1510fb50b3
Eray <142736624+turkaysoftware@users.noreply.github.com>
2024-09-17 01:48:45 +0300

    Vimera has been updated to version v24.09 v2

---
+++ Vimera/TSModules.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Vimera{
+    internal class TSModules{
+        // LINK SYSTEM
+        // ======================================================================================================
+        public class TS_LinkSystem{
+            public string
+            website_link = "https://www.turkaysoftware.com",
+            twitter_link = "https://x.com/turkaysoftware",
+            github_link = "https://github.com/turkaysoftware",
+            //
+            github_link_lt = "https://raw.githubusercontent.com/turkaysoftware/vimera/main/Vimera/SoftwareVersion.txt",
+            github_link_lr = "https://github.com/turkaysoftware/vimera/releases/latest";
+        }
+        // ======================================================================================================
+        // VERSIONS
+        public class TS_VersionEngine{
+            public string TS_SofwareVersion(int v_type, int v_mode){
+                string version_mode = "";
+                string versionSubstring = v_mode == 0 ? Application.ProductVersion.Substring(0, 5) : Application.ProductVersion.Substring(0, 7);
+                switch (v_type){
+                    case 0:
+                        version_mode = v_mode == 0 ? $"{Application.ProductName} - v{versionSubstring}" : $"{Application.ProductName} - v{Application.ProductVersion.Substring(0, 7)}";
+                        break;
+                    case 1:
+                        version_mode = $"v{versionSubstring}";
+                        break;
+                    case 2:
+                        version_mode = versionSubstring;
+                        break;
+                    default:
+                        break;
+                }
+                return version_mode;
+            }
+        }
+        // ======================================================================================================
+        // SAVE PATHS
+        public static string vimera_lf = @"v_langs";                                // Main Path
+        public static string vimera_lang_en = vimera_lf + @"\English.ini";          // English    | en
+        public static string vimera_lang_tr = vimera_lf + @"\Turkish.ini";          // Turkish    | tr
+        // ======================================================================================================
+        // VIMERA SETTINGS SAVE CLASS
+        public class TSGetLangs{
+            [DllImport("kernel32.dll")]
+            private static extern long GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+            private readonly string _saveFilePath;
+            public TSGetLangs(string filePath) { _saveFilePath = filePath; }
+            public string TSReadLangs(string episode, string settingName){
+                StringBuilder stringBuilder = new StringBuilder(512);
+                GetPrivateProfileString(episode, settingName, string.Empty, stringBuilder, 511, _saveFilePath);
+                return stringBuilder.ToString();
+            }
+        }
+        // ======================================================================================================
+        // SAVE PATHS
+        public static string ts_df = Application.StartupPath;
+        public static string ts_sf = ts_df + @"\VimeraSettings.ini";
+        public static string ts_settings_container = Path.GetFileNameWithoutExtension(ts_sf);
+        // ======================================================================================================
+        // VIMERA SETTINGS SAVE CLASS
+        public class TSSettingsSave{
+            [DllImport("kernel32.dll")]
+            private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+            [DllImport("kernel32.dll")]
+            private static extern long GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+            private readonly string _saveFilePath;
+            public TSSettingsSave(string filePath) { _saveFilePath = filePath; }
+            public string TSReadSettings(string episode, string settingName){
+                StringBuilder stringBuilder = new StringBuilder(512);
+                GetPrivateProfileString(episode, settingName, string.Empty, stringBuilder, 511, _saveFilePath);
+                return stringBuilder.ToString();
+            }
+            public long TSWriteSettings(string episode, string settingName, string value){
+                return WritePrivateProfileString(episode, settingName, value, _saveFilePath);
+            }
+        }
+        // DYNAMIC SIZE COUNT ALGORITHM
+        // ======================================================================================================
+        public static string TS_FormatSize(double bytes){
+            string[] suffixes = { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+            int suffixIndex = 0;
+            double doubleBytes = bytes;
+            while (doubleBytes >= 1024 && suffixIndex < suffixes.Length - 1){
+                doubleBytes /= 1024;
+                suffixIndex++;
+            }
+            return $"{doubleBytes:0.##} {suffixes[suffixIndex]}";
+        }
+        public static double TS_FormatSizeNoType(double bytes){
+            while (bytes >= 1024){
+                bytes /= 1024;
+            }
+            return Math.Round(bytes, 2);
+        }
+        // TITLE BAR SETTINGS DWM API
+        // ======================================================================================================
+        [DllImport("DwmApi")]
+        public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+        // ======================================================================================================
+        // DPI AWARE
+        [DllImport("user32.dll")]
+        public static extern bool SetProcessDPIAware();
+    }
+}
